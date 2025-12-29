76518b66a26e5de3fd81a6f2b10e56595f13e745
Ady Dhermawan <adydhermawan@Adys-MacBook-Pro.local>
2025-12-29 19:59:10 +0700

    fix(vercel): redirect bootstrap cache to /tmp

--- api/index.php
+++ api/index.php
@@ -19,6 +19,8 @@ if (isset($_ENV['VERCEL_ENV']) || isset($_SERVER['VERCEL_ENV'])) {
 
         // Function to recursively adjust paths
         $storagePath = '/tmp/storage';
+        $bootstrapCachePath = '/tmp/bootstrap/cache';
+
         if (!is_dir($storagePath)) {
             mkdir($storagePath, 0777, true);
             mkdir($storagePath . '/app', 0777, true);
@@ -27,6 +29,17 @@ if (isset($_ENV['VERCEL_ENV']) || isset($_SERVER['VERCEL_ENV'])) {
             mkdir($storagePath . '/framework/sessions', 0777, true);
             mkdir($storagePath . '/logs', 0777, true);
         }
+
+        if (!is_dir($bootstrapCachePath)) {
+            mkdir($bootstrapCachePath, 0777, true);
+        }
+        
+        // Redirect Laravel caches to /tmp (since project dir is read-only)
+        $_ENV['APP_SERVICES_CACHE'] = $bootstrapCachePath . '/services.php';
+        $_ENV['APP_PACKAGES_CACHE'] = $bootstrapCachePath . '/packages.php';
+        $_ENV['APP_CONFIG_CACHE'] = $bootstrapCachePath . '/config.php';
+        $_ENV['APP_ROUTES_CACHE'] = $bootstrapCachePath . '/routes.php';
+        $_ENV['APP_EVENTS_CACHE'] = $bootstrapCachePath . '/events.php';
         
         // Override standard Laravel storage path
         $app = require __DIR__ . '/../bootstrap/app.php';
