be5927ec9c1e4ae3f48fb9cfb96443c79f6a2eb8
Ady Dhermawan <adydhermawan@Adys-MacBook-Pro.local>
2025-12-30 02:34:02 +0700

    Fix PDF upload: use temp file and putFileAs strategy

--- app/Services/PdfReportService.php
+++ app/Services/PdfReportService.php
@@ -37,16 +37,18 @@ class PdfReportService
         // Clean filename (remove spaces and special chars)
         $filename = preg_replace('/[^a-zA-Z0-9_\/.-]/', '_', $filename);
 
-        // Save to storage using stream to avoid null byte error
-        $content = $pdf->output();
-        $stream = fopen('php://temp', 'r+');
-        fwrite($stream, $content);
-        rewind($stream);
+        // Save to storage using temp file and putFileAs (proven to work)
+        $tempPath = tempnam(sys_get_temp_dir(), 'report_');
+        file_put_contents($tempPath, $pdf->output());
 
-        Storage::put($filename, $stream);
+        $directory = dirname($filename);
+        $name = basename($filename);
 
-        if (is_resource($stream)) {
-            fclose($stream);
+        Storage::putFileAs($directory, new \Illuminate\Http\File($tempPath), $name);
+
+        // Cleanup
+        if (file_exists($tempPath)) {
+            unlink($tempPath);
         }
 
         // Update report record
